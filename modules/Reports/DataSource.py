1e51ac59ae3b5ffef8e0a267cf7f0a9f25353db7
nmadd <ncmaddrey@gmail.com>
2020-02-05 15:15:12 -0500

    adelete Pipelines and Reports

--- modules/Reports/DataSource.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-
-class DataSource(ABC):
-    def __init__(self, id:str, title:str, data_pipe):
-        self.id = id
-        self.title = title
-        self.data_pipe = data_pipe
-        self.df = None
