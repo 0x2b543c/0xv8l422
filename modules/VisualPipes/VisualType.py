5a7379e6026d98d9266b6ab8f320a8fc5b118a45
nmadd <ncmaddrey@gmail.com>
2020-02-06 11:35:17 -0500

    delete unneeded repos + rename research-notebooks

--- modules/VisualPipes/VisualType.py
+++
@@ -1,5 +0,0 @@
-from enum import Enum
-class VisualType(Enum):
-    LINE_CHART = 'line_chart'
-    STACKED_AREA_CHART = 'stacked_area_chart'
-    BAR_CHART = 'bar_chart'
--- modules/Visualizers/Adapters.py
+++
@@ -1,8 +0,0 @@
-def network_data_adapter(metrics=[str], assets=[str]):
-    result = []
-    for asset in assets:
-        for metric in metrics:
-            result.append(f'{asset}.{metric}') 
-    return result 
-
-
